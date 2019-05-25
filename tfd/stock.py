8033920a15df0b914b2f4fb24678cb3a4f77eb67
Antonio Camas <antoniocamas@hotmail.com>
2019-05-25 12:59:40 +0200

    Change cardstack for list in stock (Refactor)

--- tfd/stock.py
+++ tfd/stock.py
@@ -1,31 +1,30 @@
 import copy
 from tfd import exception
-from tfd.cardstack import CardStack
 from tfd.suit import Suit
 from tfd.number import Number
 
 class Stock(object):
 
     def __init__(self, cards=list()):
-        self._cardstack = CardStack(cards)
+        self._cards = cards
 
     def push(self, card):
         if card.isFaceUp():
             raise exception.InvalidCard(str(card))
         
-        self._cardstack.push(card)
+        self._cards.append(card)
         
     def pop(self):
-        return self._cardstack.pop()
+        return self._cards.pop()
 
     def peek(self):
-        return self._cardstack.peek()
+        return self._cards[-1]
 
     def empty(self):
-        return self._cardstack.empty()
+        return not self._cards
         
     def getTop(self, numberOfCards):
-        originalCards = copy.deepcopy(self._cardstack)
+        originalCards = copy.deepcopy(self._cards)
         retCards = list()
         for _ in range(numberOfCards):
             if not self.empty():
