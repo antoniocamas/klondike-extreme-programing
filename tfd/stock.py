1726c447c97dd0cc045bb039838402decfd4c817
Antonio Camas <antoniocamas@hotmail.com>
2019-05-25 15:36:19 +0200

    Extract stock getTop() simulating overload (Refactor Extract Method)

--- tfd/stock.py
+++ tfd/stock.py
@@ -18,11 +18,17 @@ class Stock(object):
 
     def empty(self):
         return not self._cards
+
+    def getTop(self, numberOfCards=None):
+        if not numberOfCards:
+            return self._getLastCards(1)[0]
         
-    def getTop(self, numberOfCards):
+        return self._getLastCards(numberOfCards)
+    
+    def _getLastCards(self, numberOfCards):
         return [x for x in reversed(self._cards[-numberOfCards:])]
 
     def removeTop(self, numberOfCards):
         for _ in range(numberOfCards):
             if not self.empty():
-                self.pop()
+                self._cards.pop()
