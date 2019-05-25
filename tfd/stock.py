1d0d180483a02f65524a96117737739445b91a81
Antonio Camas <antoniocamas@hotmail.com>
2019-05-25 12:37:51 +0200

    Change inheritance to composition in Stock (Refactor ReplaceInheritancewithDelegation)

--- tfd/stock.py
+++ tfd/stock.py
@@ -4,19 +4,28 @@ from tfd.cardstack import CardStack
 from tfd.suit import Suit
 from tfd.number import Number
 
-class Stock(CardStack):
+class Stock(object):
 
     def __init__(self, cards=list()):
-        super(Stock, self).__init__(cards)
+        self._cardstack = CardStack(cards)
 
     def push(self, card):
         if card.isFaceUp():
             raise exception.InvalidCard(str(card))
         
-        super(Stock, self).push(card)
+        self._cardstack.push(card)
+        
+    def pop(self):
+        return self._cardstack.pop()
+
+    def peek(self):
+        return self._cardstack.peek()
+
+    def empty(self):
+        return self._cardstack.empty()
         
     def getTop(self, numberOfCards):
-        originalCards = copy.deepcopy(self._cards)
+        originalCards = copy.deepcopy(self._cardstack)
         retCards = list()
         for _ in range(numberOfCards):
             if not self.empty():
