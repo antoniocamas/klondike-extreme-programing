aff84c9eb77818de73cdb17346c48b9a0ffcafd2
Antonio Camas <antoniocamas@hotmail.com>
2019-05-25 12:57:31 +0200

    Change cardstack for list in waste (Refactor)

--- tfd/waste.py
+++ tfd/waste.py
@@ -1,23 +1,22 @@
 from tfd import exception
-from tfd.cardstack import CardStack
 
 class Waste(object):
 
     def __init__(self, cards):
-        self._cardstack = CardStack(cards)
+        self._cards = cards
 
     def push(self, card):
         if not card.isFaceUp():
             raise exception.InvalidCard(str(card))
         
-        self._cardstack.push(card)
+        self._cards.append(card)
         
     def pop(self):
-        return self._cardstack.pop()
+        return self._cards.pop()
 
     def peek(self):
-        return self._cardstack.peek()
+        return self._cards[-1]
 
     def empty(self):
-        return self._cardstack.empty()
+        return not self._cards
     
